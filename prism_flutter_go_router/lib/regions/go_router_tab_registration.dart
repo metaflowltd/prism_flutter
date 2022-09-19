14e88a03be1e2871d14e828456dd2fd032fbda9d
Ariel Ben Horesh <ariel@lumen.me>
2022-09-19 13:54:18 +0300

    Change name

--- prism_flutter_go_router/lib/regions/go_router_tab_registration.dart
+++ prism_flutter_go_router/lib/regions/go_router_tab_registration.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:prism_flutter_core/prism_flutter_core.dart';
 
-class RegionWidgetRegistration extends RegionRegistration<GoRoute> {
-  RegionWidgetRegistration({required RegionMetadata metadata, required GoRoute Function() registration})
+class RegionRouterRegistration extends RegionRegistration<GoRoute> {
+  RegionRouterRegistration({required RegionMetadata metadata, required GoRoute Function() registration})
       : super(metadata: metadata, registration: registration);
 }
