6717e4001ec1e6768884900ab47224088b9ac378
Ariel Ben Horesh <ariel@lumen.me>
2022-09-19 13:53:32 +0300

    Forgot export

--- prism_flutter_go_router/lib/prism_flutter_go_router.dart
+++ prism_flutter_go_router/lib/prism_flutter_go_router.dart
@@ -3,3 +3,4 @@ library prism_flutter_go_router;
 export 'modules/go_router_bootstrapper.dart';
 export 'modules/go_router_module.dart';
 export 'regions/go_router_region_tabs_builder.dart';
+export 'regions/go_router_tab_registration.dart';
