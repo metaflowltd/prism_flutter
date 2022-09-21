6857a3aa5cfbbf41ed74975d0efdf0785891aee4
Ariel Ben Horesh <ariel@lumen.me>
2022-09-21 12:49:19 +0300

    Remove the example for now

--- prism_flutter_core/example/android/app/src/main/kotlin/com/example/prism_flutter_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.prism_flutter_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- prism_flutter_core/example/lib/events/add_event.dart
+++
@@ -1,3 +0,0 @@
-import 'package:prism_flutter_core/prism_flutter_core.dart';
-
-class AddEvent extends Event<int> {}
