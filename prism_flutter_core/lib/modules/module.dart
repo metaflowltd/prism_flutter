7324a9dc24a6d6bf61c13f28a58c94b0bda7abc4
Ariel Ben Horesh <ariel@lumen.me>
2023-01-04 15:24:53 +0200

    Adding reset to module

--- prism_flutter_core/lib/modules/module.dart
+++ prism_flutter_core/lib/modules/module.dart
@@ -1,3 +1,8 @@
 abstract class Module<T> {
+  /// Initializes a module
   Future<void> init(T container);
+
+  /// Resets a module and prepare it for another initialization call
+  /// Can be used to reset state after a user log out for instance.
+  Future<void> reset(T container) async {}
 }
