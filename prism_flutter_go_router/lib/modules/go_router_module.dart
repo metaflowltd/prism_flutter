af46b90df9693a98909420ebd6f3857983c28566
Andrii Vachasov <104822509+a-vachasov@users.noreply.github.com>
2023-09-07 12:00:35 +0300

    Make GoRouterModuleMixin to allow modules with containers different than GetIt implement routing.

--- prism_flutter_go_router/lib/modules/go_router_module.dart
+++ prism_flutter_go_router/lib/modules/go_router_module.dart
@@ -1,6 +1,9 @@
+import 'package:prism_flutter_core/modules/module.dart';
 import 'package:prism_flutter_getit/prism_flutter_getit.dart';
 import 'package:prism_flutter_go_router/interfaces/module_route.dart';
 
-abstract class GoRouterModule extends GetItModule {
+mixin GoRouterModuleMixin<T> on Module<T> {
   List<ModuleRoute> configureRoutes();
 }
+
+abstract class GoRouterModule extends GetItModule with GoRouterModuleMixin {}
